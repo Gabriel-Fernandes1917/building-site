053fd62d9315aca0e146f1c241b85e44c894f7de
Gabriel Fernandes <gabgui2001@gmail.com>
2021-04-11 05:40:27 -0300

    finished the buttons +, -, / and *.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -12,5 +12,37 @@ function add(){
     result = Number(input1) + Number(input2);
     document.querySelector('#output-cal').innerHTML = result;
 
+}
 
+function sub(){
+    var input1;
+    var input2;
+    var result;
+
+    input1 = document.getElementById('input-cal1').value;
+    input2 = document.getElementById('input-cal2').value;
+    result = Number(input1) - Number(input2);
+    document.querySelector('#output-cal').innerHTML = result;
+}
+
+function mul(){
+    var input1;
+    var input2;
+    var result;
+
+    input1 = document.getElementById('input-cal1').value;
+    input2 = document.getElementById('input-cal2').value;
+    result = Number(input1) * Number(input2);
+    document.querySelector('#output-cal').innerHTML = result;
+}
+
+function div(){
+    var input1;
+    var input2;
+    var result;
+
+    input1 = document.getElementById('input-cal1').value;
+    input2 = document.getElementById('input-cal2').value;
+    result = Number(input1) / Number(input2);
+    document.querySelector('#output-cal').innerHTML = result;
 }
