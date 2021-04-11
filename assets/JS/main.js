114c55886e52d773ffe7096ae35e33696f8d108d
Gabriel Fernandes <gabgui2001@gmail.com>
2021-04-10 22:10:59 -0300

    building calculator

---
+++ assets/JS/main.js
@@ -0,0 +1,16 @@
+
+
+//functions the calculator
+
+function add(){
+    var input1;
+    var input2;
+    var result;
+
+    input1 = document.getElementById('input-cal1').value;
+    input2 = document.getElementById('input-cal2').value;
+    result = Number(input1) + Number(input2);
+    document.querySelector('#output-cal').innerHTML = result;
+
+
+}
